0cd4e52fb1122b9c3f167bb365b6968aa4a4426f
david <tec.elec.mercado@gmail.com>
2023-05-20 05:07:25 -0300

    agregamos map centrado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Heder from "./components/layout/Heder";
-import Pruebita from "./components/Home/pruebita";
+import Pruebita from "./components/Home/Pruebita";
 import Footer from "./components/layout/Footer";
+//import Eror from "./components/pages/Eror";
+import Contac from "./components/pages/Contac";
 
 function App() {
   return (
     <>
        <Heder/>
+       
       <Pruebita />
+      <Contac/>
       <Footer/>
 
     </>
