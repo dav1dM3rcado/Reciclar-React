954fa859c334ae26a46ef5b4a9c8116756254679
unknown <tec.elec.mercado@gmail.com>
2023-05-28 12:33:32 -0300

    agregamos registar y login

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import Heder from "./components/layout/Heder";
 import Pruebita from "./components/Home/Pruebita";
 import Footer from "./components/layout/Footer";
 //import Eror from "./components/pages/Eror";
-import Contac from "./components/pages/Contac";
-
+//import Contac from "./components/pages/Contac";
+import Registrar from "./components/pages/Registrar";
+//import Login from "./components/pages/Login";
 
 function App() {
   return (
     <>
        <Heder/>
       <Pruebita />
-      <Contac/>
+      <Registrar/>
       <Footer/>
 
     </>
